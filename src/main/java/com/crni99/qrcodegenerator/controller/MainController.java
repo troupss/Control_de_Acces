9ef10c8dc5b162b6952031bd5b7f8c5de5a80a81
RaulJimix <rauljimix@gmail.com>
2024-02-16 10:50:41 +0100

    DissenyCanvi
    yeahbuddy

--- src/main/java/com/crni99/qrcodegenerator/controller/MainController.java
+++ src/main/java/com/crni99/qrcodegenerator/controller/MainController.java
@@ -1,26 +1,20 @@
 package com.crni99.qrcodegenerator.controller;
 
-import java.io.IOException;
 import java.util.Date;
 import java.time.LocalDate;
-import java.util.Optional;
 
-import com.crni99.qrcodegenerator.config.PasswordGenerator;
 import com.crni99.qrcodegenerator.models.Partits;
 import com.crni99.qrcodegenerator.models.Tickets;
 import com.crni99.qrcodegenerator.repository.PartitsRepository;
 import com.crni99.qrcodegenerator.repository.TicketsRepository;
-import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.crni99.qrcodegenerator.service.QRCodeService;
 
-import javax.servlet.http.Part;
 
 @Controller
 public class MainController {
