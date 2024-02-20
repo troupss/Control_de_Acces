c8b308ebfb1fbdfb4c8bac8c772ece1044296fe0
RaulJimix <rauljimix@gmail.com>
2024-02-20 11:47:53 +0100

    Register
    bomboclat

--- src/main/java/com/crni99/qrcodegenerator/controller/usuariController.java
+++ src/main/java/com/crni99/qrcodegenerator/controller/usuariController.java
@@ -1,9 +1,11 @@
 package com.crni99.qrcodegenerator.controller;
 
+import com.crni99.qrcodegenerator.models.Usuaris;
 import com.crni99.qrcodegenerator.repository.UsuarisRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -23,4 +25,32 @@ public class usuariController {
         model.addAttribute("error", error);
         return "mostrarUsuaris";
     }
+    @GetMapping("/registrar")
+    public String mostrarFormularioAgregar(Model model) {
+        model.addAttribute("usuari", new Usuaris());
+        return "registrarUsuaris";
+    }
+
+    @PostMapping("/guardarUsuario")
+    public String guardarUsuario(Usuaris usuario) {
+        repository.save(usuario);
+        return "redirect:/usuaris";
+    }
+
+    @GetMapping("/loguejar")
+    public String mostrarFormularioLogin() {
+        return "loguejarUsuaris";
+    }
+
+    @PostMapping("/iniciarSesion")
+    public String iniciarSesion(@RequestParam("email") String email, @RequestParam("password") String password) {
+        // Aquí deberías implementar la lógica para verificar el inicio de sesión
+        // Por ejemplo, buscar el usuario en la base de datos por el correo electrónico y verificar la contraseña
+
+        // Suponiendo que la verificación de inicio de sesión es exitosa, puedes redirigir a una página de inicio
+        return "redirect:/inicio";
+    }
+
 }
+
+
